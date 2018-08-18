3f05064a6ac3320d38fe51cde2bc685d4944ab2e
srac1777 <shashank.racherla@gmail.com>
2018-08-18 14:59:42 -0700

    fixed store

--- Frontend/Components/app.jsx
+++ Frontend/Components/app.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import NavBarContainer from './nav_bar/nav_bar_container';
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+// import NavBarContainer from './nav_bar/nav_bar_container';
 // import SignupFormContainer from './session_form/signup_form_container';
 // import SigninFormContainer from './session_form/signin_form_container';
 // import SigninFormPasswordContainer from './session_form/signin_form_password_container';
 // import SignupinNavBarContainer from './nav_bar/signupin_nav_bar_container';
 // import ProjectFormContainer from './projects/projects_form_container';
 // import ProjectShowContainer from './projects/projects_show_container';
-import ProjectIndexContainer from './projects/projects_index_container';
+// import ProjectIndexContainer from './projects/projects_index_container';
 const App = () => (
   <div className="everything">
-    <Switch>
+    {/* <Switch>
       <AuthRoute exact path="/signup" component={SignupinNavBarContainer} />
       <AuthRoute exact path="/signin" component={SignupinNavBarContainer} />
       <Route path="/projects/:projectId" component={NavBarContainer} />
@@ -24,7 +24,8 @@ const App = () => (
       <AuthRoute exact path="/signinpassword" component={SigninFormPasswordContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <Route path="/" component={ProjectIndexContainer} />
-    </Switch>
+    </Switch> */}
+    hi
   </div>
 );
 
---
+++ Frontend/Reducers/root_reducer.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+
+// import entitiesReducer from './entities_reducer';
+// import sessionReducer from './session_reducer';
+// import errorsReducer from './errors_reducer';
+// import ui from './ui_reducer';
+// import playNowReducer from './play_now_reducer';
+// import nowPlayingQueueReducer from './now_playing_queue_reducer';
+
+const rootReducer = combineReducers({
+    // entities: entitiesReducer,
+    // session: sessionReducer,
+    // errors: errorsReducer,
+    // ui,
+    // now_playing: playNowReducer,
+    // now_playing_queue: nowPlayingQueueReducer
+});
+
+export default rootReducer;
